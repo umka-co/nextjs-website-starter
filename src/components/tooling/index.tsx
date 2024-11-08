6ced7544f4eb4f2b77868f8dc2726587144c9949
Anton Karpenko <karpolan@karpolan.com>
2024-11-08 10:57:18 +0100

    fix: CodeReview

--- src/components/tooling/index.tsx
+++ src/components/tooling/index.tsx
@@ -3,4 +3,4 @@ import Advertising from './Advertising';
 import MobileOrDesktop from './MobileOrDesktop';
 import StylesInjector from './StylesInjector';
 
-export { Analytics, Advertising, MobileOrDesktop, StylesInjector as StylesInjector };
+export { Analytics, Advertising, MobileOrDesktop, StylesInjector };
--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const IS_PRODUCTION = getCurrentEnvironment() === 'production'; // Enable
 // export const IS_PRODUCTION =  process.env.NEXT_PUBLIC_ENV === 'production'; // Enables analytics, etc.
 
 // export const PUBLIC_URL = envRequired(process.env.NEXT_PUBLIC_PUBLIC_URL); // Variant 1 - value is required
-export const PUBLIC_URL = process.env.NEXT_PUBLIC_PUBLIC_URL ?? 'http://localgost:3000'; // Variant 2 - value is optional
+export const PUBLIC_URL = process.env.NEXT_PUBLIC_PUBLIC_URL ?? 'http://localhost:3000'; // Variant 2 - value is optional
 
 export const APP_NAME = 'TODO: Add Name';
 export const APP_SHORT_NAME = 'TODO: Add Short Name';
